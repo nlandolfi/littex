5c2b62dc57e2c66ea99551beb65217811784da6a
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2022-03-21 12:33:18 -0700

    checkpoint

--- cmd/parse1/parse1.go
+++ cmd/parse1/parse1.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 
@@ -10,17 +8,21 @@ import (
 )
 
 func main() {
-	bs, err := os.ReadFile("text.gba")
+	bs, err := os.ReadFile("halmos.gba")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	f, _ := gba.ParseSource1(bs)
 
-	bs, err = json.MarshalIndent(f, "", "  ")
-	if err != nil {
-		log.Fatal(err)
-	}
+	/*
+		bs, err = json.MarshalIndent(f, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	fmt.Fprint(os.Stdout, string(bs))
+		fmt.Fprint(os.Stdout, string(bs))
+	*/
+
+	f.WriteTo(os.Stdout)
 }
